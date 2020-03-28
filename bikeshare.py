559ab1f4199ecaf90e5c12139461f72da1ad3282
Anika Stephan <anika.stephan@bmw.de>
2020-03-28 16:27:00 +0100

    Changed displayed description

--- bikeshare.py
+++ bikeshare.py
@@ -117,9 +117,9 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays statistics on the most popular stations and trips."""
 
-    print('\nCalculating The Most Popular Stations and Trip...\n')
+    print('\nCalculating The Most Popular Stations and Trips...\n')
     start_time = time.time()
 
     print("The most common start station is: {} ".format(
